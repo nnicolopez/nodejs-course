3925137bcdb24d7e6b9985bcec6f161ddda83ecf
Nico López <nnicolopez@gmail.com>
2019-05-17 16:33:43 -0300

    Move html to ejs

--- routes/shop.js
+++ routes/shop.js
@@ -8,8 +8,14 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  console.log(adminData.products);
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products = adminData.products;
+  res.render('shop', {
+    pageTitle: "Shop",
+    products,
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true
+  });
 });
 
 module.exports = router;
