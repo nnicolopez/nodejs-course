4bc7b3840959eafbd7970f728b59f66ecef2b790
Nico Lopez <nnicolopez@gmail.com>
2019-06-13 21:05:04 -0300

    Add user model

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -19,16 +19,16 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('5cfeef4a4142ff50c04ab75d')
-//     .then(user => {
-//       req.user = new User(user.username, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => {
-//       console.log(err);
-//     });
-// });
+app.use((req, res, next) => {
+  User.findById('5d02e3a5a8aa1f1efadc6656')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -38,6 +38,17 @@ app.use(errorController.get404);
 // mongoose.set('useFindAndModify', false);
 mongoose.connect(uri, { useNewUrlParser: true })
   .then(() => {
+    User.findOne()
+      .then(user => {
+        if (!user) {
+          const user = new User({
+            username: 'Nico',
+            email: 'nico@test.com',
+            cart: { items: [] }
+          });
+          user.save();
+        }
+      })
     app.listen(3000);
   })
   .catch(err => {
