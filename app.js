a8107ad76ee5be5d08841ebccc61cc650949281e
Nico Lopez <nnicolopez@gmail.com>
2020-03-31 20:40:41 -0300

    Add authentication flow

--- app.js
+++ app.js
@@ -57,18 +57,6 @@ app.use(errorController.get404);
 mongoose
   .connect(MONGODB_URI, { useNewUrlParser: true })
   .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'Nico',
-          email: 'nico@test.com',
-          cart: {
-            items: []
-          }
-        });
-        user.save();
-      }
-    });
     app.listen(3000);
   })
   .catch(err => {
